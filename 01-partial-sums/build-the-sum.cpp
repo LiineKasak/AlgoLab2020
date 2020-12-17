3ac8f80afcfafce55a0216f00f151806777a9396
Liine Kasak <liine.kasak@gmail.com>
2020-12-17 16:08:29 +0100

    week 1

---
+++ 01-partial-sums/build-the-sum.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+void solve() {
+  int n, s = 0, t; cin >> n;
+  for (int i = 0; i < n; i++) {
+    cin >> t;
+    s += t;
+  }
+  cout << s << endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  
+  int t; cin >> t;
+  for (int i = 0; i < t; i++) {
+    solve();
+  }
+}
---
+++ 01-partial-sums/dominoes.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void solve() {
+  int dominoes_nr; cin >> dominoes_nr;
+  
+  vector<int> domino_heights(dominoes_nr);
+  for (int i = 0; i < dominoes_nr; i++)
+    cin >> domino_heights[i];
+  
+  int falling_dominoes = 1;
+  int curr, prev = domino_heights[0];
+  
+  for (int i = 1; i < dominoes_nr; i++) {
+    if (prev <= 1) break;
+    falling_dominoes++;
+    curr = domino_heights[i];
+    prev--;
+    if (curr > prev) prev = curr;
+  }
+  cout << falling_dominoes << endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  int t; cin >> t;
+  while (t--) solve();
+  return 0;
+}
